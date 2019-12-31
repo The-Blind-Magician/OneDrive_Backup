34eef9ac8aa40d3614fd81e9bbfab1b8219ff9ee
Christian <christian220h@outlook.com>
2019-12-31 12:13:07 -0500

    Cleaned code and Added Filepath exemption with '*'

--- Program.cs
+++ Program.cs
@@ -9,30 +9,22 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Diagnostics;
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
 namespace OneDrive_Backup
 {
     class Program
-    {
-        
+    {        
         static string directoryPaths = AppDomain.CurrentDomain.BaseDirectory + "\\Directories to Copy.txt";
-        static string version = AppDomain.CurrentDomain.BaseDirectory + "\\Version.txt";
-        static string exe0Path = AppDomain.CurrentDomain.BaseDirectory + "\\OneDrive_Backup.exe";
-        static string exe1Path = AppDomain.CurrentDomain.BaseDirectory + "\\OneDrive_Backup1.exe";
-        static string exe2Path = AppDomain.CurrentDomain.BaseDirectory + "\\OneDrive_Backup2.exe";
-        static string exeName = Path.GetFileName(Assembly.GetEntryAssembly().Location);
 
         static UInt64 fileCount = 0;
         static UInt64 modCount = 0;
         static UInt64 unMod = 0;
         static UInt64 created = 0;
-
-
+        
         static void Main()
         {
-            
-            if (!updateExecutable())
+            if (!Update.updateExecutable())
                 return;
             string[] directories;
             if (!File.Exists(directoryPaths))
@@ -117,79 +109,17 @@ namespace OneDrive_Backup
 
         private static string[] buildDirectories(string path)
         {
-            return (File.ReadAllLines(path));
-        }
+            string[] preProc = File.ReadAllLines(path);
+            List<string> postProc = new List<string>();
 
-        private static bool updateExecutable()
-        {
-            string txtVer;
-            using (var ctxt = new WebClient())
+            foreach(string s in preProc)
             {
-                Uri uri = new Uri("https://github.com/The-Blind-Magician/OneDrive_Backup/raw/master/Version.txt");
-                txtVer = ctxt.DownloadString(uri);
-                System.Threading.Thread.Sleep(500);
-            }
-            if ((exeName != "OneDrive_Backup2.exe") && 
-                Assembly.GetExecutingAssembly().GetName().Version.ToString().Trim() == txtVer.Trim())
-            {
-                if (File.Exists(exe2Path))
+                if(!s.ToArray().First().Equals('*'))
                 {
-                    File.SetAttributes(exe2Path, FileAttributes.Normal);
-                    File.Delete(exe2Path);
-                }
-                if (File.Exists(exe1Path))
-                {
-                    File.SetAttributes(exe1Path, FileAttributes.Normal);
-                    File.Delete(exe1Path);
-                }
-                File.Delete(version);
-                return true;
-            }
-            try
-            {
-                if (exeName == "OneDrive_Backup.exe")
-                {
-                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-                    using (var client = new WebClient())
-                    {                        
-                        if (!File.Exists(exe1Path))
-                        {
-                            Uri uri = new Uri("https://github.com/The-Blind-Magician/OneDrive_Backup/raw/master/bin/Debug/OneDrive_Backup.exe");
-                            client.DownloadFile(uri, "OneDrive_Backup1.exe");
-                        }
-                        File.SetAttributes(exe1Path, FileAttributes.Normal);
-                        File.Copy(exe1Path, exe2Path, true);
-                        File.SetAttributes(exe2Path, FileAttributes.Normal);
-
-                        File.SetAttributes(exe2Path, FileAttributes.Normal);
-                        ProcessStartInfo info = new ProcessStartInfo(exe2Path);
-                        info.UseShellExecute = true;
-                        info.Verb = "runas";
-                        Process.Start(info);
-
-                        return false;
-                    }
-                }
-                else if (exeName == "OneDrive_Backup2.exe")
-                {
-                    System.Threading.Thread.Sleep(1000);
-                    File.SetAttributes(exe0Path, FileAttributes.Normal);
-                    File.Delete(exe0Path);        
-                    
-                    File.Move(exe1Path, exe0Path);
-                    File.SetAttributes(exe0Path, FileAttributes.Normal);
-
-                    Process.Start(exe0Path);
-                    return false;
+                    postProc.Append(s);
                 }
             }
-            catch(Exception e)
-            {
-                Console.WriteLine(e);
-                Console.Read();
-                return false;
-            }
-            return false;
-        }
+            return (postProc.ToArray());
+        }  
     }
 }
---
+++ Update.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Application;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Diagnostics;
+
+namespace OneDrive_Backup
+{
+    public class Update
+    {
+        static string version = AppDomain.CurrentDomain.BaseDirectory + "\\Version.txt";
+        static string exe0Path = AppDomain.CurrentDomain.BaseDirectory + "\\OneDrive_Backup.exe";
+        static string exe1Path = AppDomain.CurrentDomain.BaseDirectory + "\\OneDrive_Backup1.exe";
+        static string exe2Path = AppDomain.CurrentDomain.BaseDirectory + "\\OneDrive_Backup2.exe";
+        static string exeName = Path.GetFileName(Assembly.GetEntryAssembly().Location);
+
+        public static bool updateExecutable()
+        {
+            string txtVer;
+            using (var ctxt = new WebClient())
+            {
+                Uri uri = new Uri("https://github.com/The-Blind-Magician/OneDrive_Backup/raw/master/Version.txt");
+                txtVer = ctxt.DownloadString(uri);
+                System.Threading.Thread.Sleep(500);
+            }
+            if ((exeName != "OneDrive_Backup2.exe") &&
+                Assembly.GetExecutingAssembly().GetName().Version.ToString().Trim() == txtVer.Trim())
+            {
+                if (File.Exists(exe2Path))
+                {
+                    File.SetAttributes(exe2Path, FileAttributes.Normal);
+                    File.Delete(exe2Path);
+                }
+                if (File.Exists(exe1Path))
+                {
+                    File.SetAttributes(exe1Path, FileAttributes.Normal);
+                    File.Delete(exe1Path);
+                }
+                File.Delete(version);
+                return true;
+            }
+            try
+            {
+                if (exeName == "OneDrive_Backup.exe")
+                {
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                    using (var client = new WebClient())
+                    {
+                        if (!File.Exists(exe1Path))
+                        {
+                            Uri uri = new Uri("https://github.com/The-Blind-Magician/OneDrive_Backup/raw/master/bin/Debug/OneDrive_Backup.exe");
+                            client.DownloadFile(uri, "OneDrive_Backup1.exe");
+                        }
+                        File.SetAttributes(exe1Path, FileAttributes.Normal);
+                        File.Copy(exe1Path, exe2Path, true);
+                        File.SetAttributes(exe2Path, FileAttributes.Normal);
+
+                        File.SetAttributes(exe2Path, FileAttributes.Normal);
+                        ProcessStartInfo info = new ProcessStartInfo(exe2Path);
+                        info.UseShellExecute = true;
+                        info.Verb = "runas";
+                        Process.Start(info);
+
+                        return false;
+                    }
+                }
+                else if (exeName == "OneDrive_Backup2.exe")
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    File.SetAttributes(exe0Path, FileAttributes.Normal);
+                    File.Delete(exe0Path);
+
+                    File.Move(exe1Path, exe0Path);
+                    File.SetAttributes(exe0Path, FileAttributes.Normal);
+
+                    Process.Start(exe0Path);
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.Read();
+                return false;
+            }
+            return false;
+        }
+    }
+}
