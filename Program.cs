399c3559ef24b7f0868188415d5d5c770d76a91f
Christian <christian220h@outlook.com>
2019-12-31 21:49:08 -0500

    Supress Jekyll

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
 namespace OneDrive_Backup
 {
     class Program
